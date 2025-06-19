fd7795341b3f75133eba5b4da416a79c989bcb13
Mohamed Gamal Mahmoud Abdelrehem <112729132+MGM33@users.noreply.github.com>
2025-06-19 21:51:48 +0300

    Update MedicineGrid.tsx

--- src/components/medicines/MedicineGrid.tsx
+++ src/components/medicines/MedicineGrid.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import DogMedicineCard from './DogMedicineCard';
 import CatMedicineCard from './CatMedicineCard';
@@ -46,13 +45,13 @@ const MedicineGrid: React.FC<MedicineGridProps> = ({ petType, selectedCategory =
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-      {medicineIds.map((id) => (
+      {medicineIds.map((id) =>
         petType === 'dogs' ? (
           <DogMedicineCard key={id} medicineId={id.toString()} />
         ) : (
           <CatMedicineCard key={id} medicineId={id.toString()} />
         )
-      ))}
+      )}
     </div>
   );
 };
