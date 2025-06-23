bde5dffdc9f4f840074b30357d87c858ae99dabb
Mohamed Gamal Mahmoud Abdelrehem <112729132+MGM33@users.noreply.github.com>
2025-06-23 10:33:29 +0300

    Update Navbar.tsx

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -87,7 +87,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
     <Link to="/" className="flex items-center space-x-3 group h-12">
- <div className="scale-[1.4]">
+ <div className="scale-[1.6]">
     <img
       src="https://videos.openai.com/vg-assets/assets%2Ftask_01jydv20e9e66t1jdcyfkzgbjc%2F1750663815_img_0.webp?st=2025-06-23T05%3A41%3A16Z&se=2025-06-29T06%3A41%3A16Z&sks=b&skt=2025-06-23T05%3A41%3A16Z&ske=2025-06-29T06%3A41%3A16Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=3d249c53-07fa-4ba4-9b65-0bf8eb4ea46a&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=GRN1AZrWoRPYVwoF2te6NTDV3QYR9wmhtzv6Dc6pUpg%3D&az=oaivgprodscus"
       alt="VetCare Logo"
